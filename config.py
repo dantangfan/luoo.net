061bf89b1dc84501886fdfb663063903a1200e3d
dantangfan <dantangfan@gmail.com>
2015-06-04 16:51:53 +0800

    rm useless

--- config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-
-keys = ['j', 'k', '-', '=', 'a', 's', 'd', 'n', 'b', 'q', '\r', ' ']
-window_keys = ['j', 'k', 'a', 's', 'd', 'n', 'b', 'q', '\r']
-
-def suicide():
-    os.system('kill `pgrep luoo.py`')
