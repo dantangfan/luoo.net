9f418484d86a5c29976a135fc0f6eef5971cb3b3
dantangfan <dantangfan@gmail.com>
2015-06-05 10:53:43 +0800

    fix bug

--- luoo/luoo.py
+++ luoo/luoo.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     format="%(asctime)s - \
 [%(process)d]%(filename)s:%(lineno)d - %(levelname)s: %(message)s",
     datefmt='%Y-%m-%d %H:%I:%S',
-    filename=os.path.expanduser('~/.doubanfm.log'),
+    filename=os.path.expanduser('~/.luoo.log'),
     level=logging.WARNING
 )
 
